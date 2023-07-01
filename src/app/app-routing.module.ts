1560018d5a33c04b41adb41bc6c50d70358aa8e5
dorsaf-may <124609467+dorsaf-may@users.noreply.github.com>
2023-07-01 18:44:13 +0100

    init-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
+import { LoginComponent } from './layout/login/login.component';
+import { FactureComponent } from './layout/facture/facture.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+
+  },
+  {
+    path: 'facture',
+    component: FactureComponent
+  },
+  {
+    path: 'login',
+    component:LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
+import { LoginComponent } from './layout/login/login.component';
+import { AppShellComponent } from './component/app-shell/app-shell.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { FactureComponent } from './layout/facture/facture.component';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon'
+import {MatButtonModule} from '@angular/material/button'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    AppShellComponent,
+    NavbarComponent,
+    FactureComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
